238eb82bf4fda6273c9ea75fa24ea57d239e3894
Agustin <jagustinbassi@gmail.com>
2020-06-26 21:47:23 -0300

    Execute the HTTP request using fields in form and do polling request

--- cms-app/js/main.js
+++ cms-app/js/main.js
@@ -48,46 +48,131 @@
 
 //=======[ Settings & Data ]===================================================
 
-var URI = "http://localhost:5000/api/v1/ibeacons_settings/";
+var DEFAULT_URL = "http://localhost:5000/api/v1/ibeacons_settings/";
 
 var http_handler = new XMLHttpRequest();
 
+var interval = null;
+
 //=======[ Utils ]=============================================================
 
-function parse_server_response(server_response) {
+function send_data_to_view(server_response) {
     var json_response = JSON.parse(server_response);
     console.log("Data received is: " + server_response);
     document.getElementById("logs_textarea").innerHTML = server_response;
 }
 
+function append_data_to_view(server_response) {
+    current_value = document.getElementById("logs_textarea").value; 
+    document.getElementById("logs_textarea").innerHTML = server_response + "\n" + current_value;
+}
+
+function clear_view_data(){
+    console.log("Clearing view data")
+    document.getElementById("logs_textarea").innerHTML = "";
+}
+
 function log_current_settings(){
-    url           = document.getElementById("post_url").value;
-    data          = document.getElementById("post_data").value;
+    request_url   = document.getElementById("request_url").value;
+    request_data  = document.getElementById("request_data").value;
     poll_checkbox = document.getElementById("poll_checkbox").checked;
     poll_secs     = document.getElementById("poll_secs").value;
     http_method   = document.getElementById("http_method").value;
 
-    console.log("url:           " + url);
-    console.log("data:          " + data);
+    console.log("request_url:   " + request_url);
+    console.log("request_data:  " + request_data);
     console.log("poll_checkbox: " + poll_checkbox);
     console.log("poll_secs:     " + poll_secs);
     console.log("http_method:   " + http_method);
-
 }
+
+function get_element_value(element_to_get){
+    if (element_to_get == "request_url" ||
+        element_to_get == "request_data" ||
+        element_to_get == "poll_secs" ||
+        element_to_get == "http_method"){
+        return document.getElementById(element_to_get).value;
+    } else if (element_to_get == "poll_checkbox"){
+        return document.getElementById(element_to_get).checked;
+    } else {
+        return null;
+    }
+}
+
+function clear_poll_interval(){
+    if(interval != null){
+        clearInterval(interval);
+        interval = null;
+    }
+}
+
+function is_interval_set(){
+    return interval != null;
+}
+
+
+
 
 //=======[ Module functions ]==================================================
 
 function execute_http_request(){
 
+    // at first los current settings
     log_current_settings();
 
+    // obtain the HTML elements in a friendly way
+    request_url   = get_element_value("request_url");
+    request_data  = get_element_value("request_data");
+    poll_checkbox = get_element_value("poll_checkbox");
+    poll_secs     = get_element_value("poll_secs");
+    http_method   = get_element_value("http_method");
+
+    // callback when HTTP request is done
     http_handler.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            parse_server_response(http_handler.responseText);
+        if (this.readyState == 4 && (this.status == 200 || this.status == 201 )) {
+            if (is_interval_set()){
+                append_data_to_view(http_handler.responseText);
+            } else {
+                send_data_to_view(http_handler.responseText);
+            }
+        } else{
+            // alert("The server has returned an error code");
         }
     };
-    
-    http_handler.open("GET", URI, true);
-    http_handler.setRequestHeader('Accept', 'application/json');
-    http_handler.send();
+
+    // clear view in order to start new log session
+    clear_view_data();
+
+    // clear interval if exists
+    clear_poll_interval();
+
+    // evaluate HTTP method
+    if(http_method.toLowerCase() == "get"){
+
+        http_handler.open("GET", request_url, true);
+        http_handler.setRequestHeader('Accept', 'application/json');
+        http_handler.send();
+
+        if(poll_checkbox == true){
+
+            console.log("Executing poll request for seconds " + poll_secs)
+            
+            poll_secs = parseInt(poll_secs);
+            
+            interval = setInterval(function(){
+                http_handler.open("GET", request_url, true);
+                http_handler.setRequestHeader('Accept', 'application/json');
+                http_handler.send();
+            }, poll_secs * 1000);
+        }
+
+    } else if(http_method.toLowerCase() == "post"){
+
+    } else if(http_method.toLowerCase() == "put"){
+
+    } else {
+        // alert("Unsupported HTTP Method");
+    }
 }
+
+//=======[ End of file ]=======================================================
