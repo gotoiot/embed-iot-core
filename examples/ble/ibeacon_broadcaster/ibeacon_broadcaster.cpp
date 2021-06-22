2df863927db8e36637b38ce045aff2dc167990cd
Agustin <jagustinbassi@gmail.com>
2021-06-22 16:25:07 -0300

    Enhace application separating it in function and ordering its execution

--- examples/ble/ibeacon_broadcaster/ibeacon_broadcaster.cpp
+++ examples/ble/ibeacon_broadcaster/ibeacon_broadcaster.cpp
@@ -34,7 +34,7 @@ SOFTWARE.*/
 #define LED_DELAY           500
 #define DEEP_SLEEP_SECONDS  10
 #define ADVERTISING_DELAY   100
-
+#define BLE_TX_POWER        0
 #define DEVICE_NAME         "Goto IoT iBeacon ESP32 001"
 #define BEACON_UUID         "8ec76ea3-6668-48da-9866-75be8bc86f4d"
 
@@ -42,6 +42,8 @@ SOFTWARE.*/
 
 RTC_DATA_ATTR static uint32_t BootCount;
 BLEAdvertising *BleAdvertising;
+static uint32_t BeaconMajor = 0;
+static uint32_t BeaconMinor = 0;
 
 /*==================[internal functions declaration]=========================*/
 
@@ -54,15 +56,25 @@ void App_Loop(void);
 
 /*==================[internal functions definition]==========================*/
 
-void Ble_SetIBeaconAdvertising(){
+void Core_SetEsp32ToDeepSleep(){
+    esp_deep_sleep(1000000LL * DEEP_SLEEP_SECONDS);
+}
+
+void Serial_PrintSeparator(){
+    Serial.print("\n\n\r======================================\n\n\r");
+}
+
+void Ble_SetDeviceAsIBeacon(uint32_t major, uint32_t minor, uint32_t txPower){
+    BLEDevice::init(DEVICE_NAME);
+    BleAdvertising = BLEDevice::getAdvertising();
     // creates iBeacon broadcaster
     BLEBeacon iBeacon = BLEBeacon();
     // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
     iBeacon.setManufacturerId(0x4C00);
     iBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-    iBeacon.setMajor((BootCount & 0xFFFF0000) >> 16);
-    iBeacon.setMinor(BootCount & 0xFFFF);
-    // iBeacon.setSignalPower(-58);
+    iBeacon.setMajor(major);
+    iBeacon.setMinor(minor);
+    iBeacon.setSignalPower(txPower);
     BLEAdvertisementData advertisementData = BLEAdvertisementData();
     // BR_EDR_NOT_SUPPORTED 0x04
     advertisementData.setFlags(0x04);
@@ -74,41 +86,48 @@ void Ble_SetIBeaconAdvertising(){
     advertisementData.addData(strServiceData);
     // set BLE advertising accordingly
     BleAdvertising->setAdvertisementData(advertisementData);
-    /** Advertising mode. Can be one of following constants:
-     *  - BLE_GAP_CONN_MODE_NON (non-connectable; 3.C.9.3.2).
-     *  - BLE_GAP_CONN_MODE_DIR (directed-connectable; 3.C.9.3.3).
-     *  - BLE_GAP_CONN_MODE_UND (undirected-connectable; 3.C.9.3.4).
-     */
     BleAdvertising->setAdvertisementType(BLE_GAP_CONN_MODE_NON);
 }
 
-void App_Init(){
-    // Configure serial port at 115200 baudrios
-    Serial.begin(SERIAL_BAURDATE);
-    // Configure pins of buttons and leds
-    pinMode(LED_ONBOARD, OUTPUT);
-    // print informative message to serial console
-    Serial.println("\n\r======================================\n\r");
-    Serial.println("\n\rWelcome to BLE iBeacon Broadcaster - www.gotoiot.com");
-    Serial.printf("Device has sent its iBeacon package %d times\n\r", BootCount++);
-    // Create the BLE Device
-    BLEDevice::init(DEVICE_NAME);
-    BleAdvertising = BLEDevice::getAdvertising();
-    Ble_SetIBeaconAdvertising();
-    // Start advertising
+void Ble_PerformAdvertisingCycle(){
+    Serial.printf("iBeacon data: {\"uuid\": %s, \"major\": %d, \"minor\": %d, \"tx_power\": %d}\n\r",
+        BEACON_UUID, BeaconMajor, BeaconMinor, BLE_TX_POWER);
     BleAdvertising->start();
-    Serial.println("Starting to advertise iBeacon package");
     delay(ADVERTISING_DELAY);
     BleAdvertising->stop();
-    Serial.printf("Stopping to advertise iBeacon package");
-    Serial.printf("Entering in deep sleep mode for %d seconds", DEEP_SLEEP_SECONDS);
-    Serial.println("\n\n\r======================================\n\r");
-    Serial.println("\n\rESP32 Bootloader data\n\r");
-    esp_deep_sleep(1000000LL * DEEP_SLEEP_SECONDS);
 }
 
+void App_ShowWelcomeMessage(){
+    Serial_PrintSeparator();
+    Serial.printf("Welcome to BLE iBeacon Broadcaster - www.gotoiot.com\n\r");
+    Serial.printf("iBeacon device has sent packages for %d times\n\r", BootCount);
+}
+
+void App_ShowStopAdvertisingMessage(){
+    Serial.printf("Stopping to advertise iBeacon package\n\r");
+    Serial.printf("Entering in deep sleep mode for %d seconds\n\r", DEEP_SLEEP_SECONDS);
+    Serial_PrintSeparator();
+}
+
+void App_UpdateIBeaconData(){
+    BootCount += 1;
+    BeaconMajor = (BootCount & 0xFFFF0000) >> 16;
+    BeaconMinor = BootCount & 0xFFFF;
+}
+
+void App_Init(){
+    Serial.begin(SERIAL_BAURDATE);
+    pinMode(LED_ONBOARD, OUTPUT);
+    App_ShowWelcomeMessage();
+    App_UpdateIBeaconData();
+    Ble_SetDeviceAsIBeacon(BeaconMajor, BeaconMinor, BLE_TX_POWER);
+}
+
+
 void App_Loop(){
-    // nothing to do
+    Ble_PerformAdvertisingCycle();
+    App_ShowStopAdvertisingMessage();
+    Core_SetEsp32ToDeepSleep();
 }
 
 /*==================[external functions definition]==========================*/
